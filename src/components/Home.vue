7cc12f3e7bd1af9a49f40a81de539b687fba9098
折影轻梦 <776194970@qq.com>
2019-01-20 15:56:08 +0800

    pwa

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,13 +27,13 @@ export default {
   data () {
     return {
       items: [
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'},
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'},
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'},
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'},
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'},
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'},
-        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'776194970',category:'这是一个分类',article:'这是一段内容'}]
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'},
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'},
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'},
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'},
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'},
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'},
+        {avatar:'https://avatar.dawnlab.me/qq/776194970',name:'折影轻梦',qq:'2019年1月19日',category:'这是一个分类',article:'这是一段内容'}]
     }
   }
 }
