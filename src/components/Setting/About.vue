4f0acad3c6d394c14d648d2a1d8371fb6bc14522
折影轻梦 <776194970@qq.com>
2019-01-20 23:19:32 +0800

    Update About.vue

--- src/components/Setting/About.vue
+++ src/components/Setting/About.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="nexmoe-setting">
       <ul>
-        <li>作者：折影轻梦</li>
-        <li>QQ群：797996017</li>
-        <li>联系QQ：776194970</li>
-        <li>Github：https://github.com/nexmoe/wall-public</li>
+        <li>作者名称：折影轻梦</li>
+        <li>企鹅群聊：797996017</li>
+        <li>联系企鹅：776194970</li>
+        <li>开源地址：https://github.com/nexmoe/wall-public</li>
         <li>版权协议：MIT</li>
       </ul>
     </div>
